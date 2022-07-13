2be036fbfd5e23ba5ed5bf3e4a620dfb0c965b35
sid-FBLA <sid20102004@gmail.com>
2022-07-13 19:17:56 -0400

    commit

--- blockchain.py
+++ blockchain.py
@@ -11,30 +11,15 @@ class Block:
         self.previousHash = previousHash
         self.hash = self.calculateHash()
 
-    def calculateHash(self):
-        return str(hashlib.sha256(str(self.index).encode('utf-8') + str(self.previousHash).encode('utf-8') + str(self.timestamp).encode('utf-8') + json.dumps(self.data).encode('utf-8')))
+    def updateHash(*args):
+        hashText = ""
+        hash = hashlib.sha256()
+        for arg in args
+            hashText += str(arg)
 
-    def printBlock(self):
-        return str(self.index) + " " + str(self.timestamp) + " " + str(self.data) + " " + str(self.previousHash) + " " + str(self.hash)
+        hash.update(hashText.encode('utf-8'))
+        return hash.hexdigest()
 
+print(updateHash("Sid"))
 
-class Blockchain:
-    def __init__(self):
-        self.chain = [self.createGenesisBlock()]
-    
-    def createGenesisBlock(self):
-        return Block(0, "07/12/2022", "Genesis Block", "0")
-    
-    def getLatestBlock(self):
-        return self.chain[len(self.chain) - 1]
-
-    def addBlock(self, newBlock):
-        newBlock.previousHash = self.getLatestBlock().hash
-        newBlock.hash = newBlock.calculateHash()
-        self.chain.append(newBlock)
-
-sidCoin = Block(1, "07/12/2022", 4)
-sidCoin.addBlock(Block(1, "07/12/2022", 4))
-#sidCoin.addBlock(Block(2, "07/12/2022", 7))
-
-print(sidCoin.printBlock())
+    def calculateHash():
