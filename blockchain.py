a4ab5c9942723d14123db7ffd7640ef703a4e201
sid-FBLA <sid20102004@gmail.com>
2022-07-13 21:42:11 -0400

    blockchain created

--- blockchain.py
+++ blockchain.py
@@ -1,25 +1,69 @@
 from copyreg import constructor
 from dataclasses import dataclass
+from datetime import date
 import hashlib
 import json
 
-class Block:
-    def __init__(self, index, timestamp, data, previousHash=''):
-        self.index = index
-        self.timestamp = timestamp
-        self.data = data
-        self.previousHash = previousHash
-        self.hash = self.calculateHash()
+def updateHash(*args):
+    hashText = ""
+    hash = hashlib.sha256()
+    for arg in args:
+        hashText += str(arg)
 
-    def updateHash(*args):
-        hashText = ""
-        hash = hashlib.sha256()
-        for arg in args
-            hashText += str(arg)
-
-        hash.update(hashText.encode('utf-8'))
-        return hash.hexdigest()
+    hash.update(hashText.encode('utf-8'))
+    return hash.hexdigest()
 
 print(updateHash("Sid"))
 
-    def calculateHash():
+
+class Block:
+    def __init__(self, data):
+        self.nonce = 0
+        self.timestamp = str(date.today())
+        self.data = data
+        self.previousHash = "0" * 64
+        self.hash = self.calculateHash()
+
+    def calculateHash(self):
+        return updateHash(self.nonce, self.timestamp, self.data, self.previousHash)
+
+    def __str__(self):
+        return str(
+            "Nonce: " + str(self.nonce) + "\n" +
+            "Timestamp: " + self.timestamp + "\n" +
+            "Data: " + self.data + "\n" +
+            "Hash: " + str(self.hash) + "\n"
+            "Previous Hash: " + self.previousHash)
+
+
+    def getHash(self):
+        return self.hash
+
+class BlockChain:
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
+        self.chain = []
+
+    def addBlock(self, block):
+        self.chain.append(block)
+
+    def mineBlock(self, block):
+        try:
+            block.previousHash = self.chain[-1].getHash()
+        except IndexError:
+            pass
+
+        while True:
+            if block.calculateHash()[:self.difficulty] == "0" * self.difficulty:
+                block.hash = block.calculateHash()
+                self.addBlock(block)
+                break
+            else:
+                block.nonce += 1
+
+blockchain = BlockChain(4)
+database = ["Audienceology", "Crypto", "Ad Fraud", "BlockChain for Ad Fraud"]
+for data in database:
+    blockchain.mineBlock(Block(data))
+for block in blockchain.chain:
+    print(block)
